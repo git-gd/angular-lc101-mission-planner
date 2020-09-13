5a90131706da8a59b03caa58fedd58686b549a0a
Randy Parker <randallbparker@gmail.com>
2020-09-13 17:28:15 -0500

    updated crew and css

--- mission-planner-dashboard/src/app/header/header.component.ts
+++ mission-planner-dashboard/src/app/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  misisonName: string = "Mars 2030";
+  missionName: string = "Mars 2030";
   rocketName: string = "Plasma Max";
 
   constructor() { }
